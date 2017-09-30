ed1719cb3c5acb2c2b3c673dc5dee93cdafcb134
SimonJonsson <simonjonsson3@hotmail.com>
2017-09-30 15:57:06 +0200

    Added lab5shiny docs

--- R/app.R
+++ R/app.R
@@ -1,14 +1,3 @@
-#' @title Visual Interaction Tool For Swedish Election Results
-#'
-#' @description
-#' Implements an interactive tool for the Swedish election results where you can
-#' see the results for each muniplicity. The default select is Stockholms Kommun.
-#' Uses the elect_viz package
-#'
-#' @name lab5shiny
-#' @docType package
-NULL
-
 devtools::install_github("SimonJonsson/lab5")
 library(lab5)
 
---
+++ R/lab5shiny.R
@@ -0,0 +1,10 @@
+#' @title Visual Interaction Tool For Swedish Election Results
+#'
+#' @description
+#' Implements an interactive tool for the Swedish election results where you can
+#' see the results for each muniplicity. The default select is Stockholms Kommun.
+#' Uses the elect_viz package
+#'
+#' @name lab5shiny
+#' @docType package
+NULL
